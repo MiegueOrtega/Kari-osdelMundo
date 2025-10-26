17d14b93aa87ec690f3d6071f2351511cfdec36c
MiegueOrtega <ortega.miguel.desarrollo@gmail.com>
2025-10-26 15:21:38 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
 
+app.use(express.json());
+app.use(express.static('public'));
+
+const regaloRoutes = require('./routes/regalos');
+app.use('/api/regalos', regaloRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor activo en http://localhost:${PORT}`);
+});
