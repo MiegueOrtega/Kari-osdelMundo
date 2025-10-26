a293fb48838b29128eafe2cdc5ac20a6ae8971af
MiegueOrtega <ortega.miguel.desarrollo@gmail.com>
2025-10-26 15:27:58 -0300

    Create regalosControllers.js

---
+++ src/controllers/regalosControllers.js
@@ -0,0 +1,21 @@
+{
+  "name": "karinodelmundo",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "start": "node src/app.js",
+    "dev": "nodemon src/app.js",
+    "test": "echo \"Error: No hay test definidos aún\" && exit 1"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "dotenv": "^17.2.3",
+    "express": "^5.1.0"
+  },
+  "devDependencies": {
+    "nodemon": "^3.1.10"
+  }
+}
